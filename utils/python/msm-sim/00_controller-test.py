13374b8e7792bac777d8c4a67746afc6f36fdf85
Edgardo Marchi <edgardo.marchi@gmail.com>
2023-02-24 21:31:48 -0300

    Fixed python simulation

--- utils/python/msm-sim/00_controller-test.py
+++ utils/python/msm-sim/00_controller-test.py
@@ -1,20 +1,27 @@
-from msm import msm
+from msm_controller import msm_c
 from simpy import core
 from memory_module import mem
 from point_adder import p_add
 from scalars import scalars as s
 from randpoints import points as rp
 from collections import deque
+from math import ceil
+from random import randint
+
+#muliscalar_multiplication = MSM()
 
 env = core.Environment()
 point_adder = p_add(env)
-memory = mem(29, 9)
-
+memory = mem(22, 12, 2**4)
 
 ## Estaria bueno poner un for para generar multiples memorias de diferentes tamanios y de ahi 
-multiscalar = msm(env, point_adder, memory)
+multiscalar = msm_c(env, point_adder, memory, 5,(0, 21), 22)
 mp = deque(rp)
-ms = deque(s)
+ms = deque()
 
+for _ in range(len(mp)):
+    ms.append(s[randint(0, 99999)])
+    
 env.process(multiscalar.bucket_aggregation(mp, ms))
 env.run()
+
--- utils/python/msm-sim/utils.py
+++ utils/python/msm-sim/utils.py
@@ -1,4 +1,21 @@
 from dataclasses import dataclass
+from enum import Enum
+from math import ceil
+
+class mem_location(Enum):
+    default = 0
+    g = 1
+    sum = 2
+
+class loop(Enum):
+    LOOP_1 = 1
+    LOOP_2 = 2
+    LOOP_3 = 3
+
+class aggregation(Enum):
+    INPUT_POINTS = 1
+    FIFO_POINTS = 2
+    END = 3
 
 @dataclass
 class Point:
@@ -7,26 +24,25 @@ class Point:
     z: int
 
 @dataclass
+class hazard:
+    point: Point 
+    bucket: int
+
+@dataclass
 class status_w:
     IS_BUSY: bool
     IS_EMPTY: bool
 
 @dataclass
-class hazard:
-    point : Point 
-    bucket_addr : int 
+class bucket_address:
+    c: int
+    k: int
 
 ## CONSTANTS
 PADD_PIPELINE_DEPTH = 52
 Q = 258664426012969094010652733694893533536393512754914660539884262666720468348340822774968888139573360124440321458177
 R = 8444461749428370424248824938781546531375899335154063827935233455917409239041
 CLK = 1
-
-@dataclass
-class Projective_Point:
-    x: int
-    y: int
-    z: int
 
 ## Auxiliary operations for point adder
 def mod_add(a, b):
@@ -41,10 +57,25 @@ def mod_mul(a, b):
 def split(s, K):
     tmp = s
     l = []
-    for i in range(0, K):
+    for _ in range(0, K):
         l.append(tmp % 2**K)
         tmp = tmp >> K
     return l
+
+def splits_per_padds(n_padds, c):
+    windows = ceil(253/ c)
+
+    sc = []
+
+    n_scalars = ceil(windows/n_padds)
+
+    i = 0
+    while i + n_scalars < windows:
+        sc.append((i, i + n_scalars - 1))
+        i += n_scalars
+
+    sc.append((i, windows - 1))
+    return sc
 
 def split_scalars(scalar, K, c):
     tmp = format(scalar, '0'+str(K*c)+'b')
@@ -52,7 +83,7 @@ def split_scalars(scalar, K, c):
     i = K*c - 1
     while i >= 0:
         tmpnum = ''
-        for a in range(c):
+        for _ in range(c):
             tmpnum = tmp[i] + tmpnum
             i -= 1
         l.append(int(tmpnum, 2))
